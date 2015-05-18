8472f4ed97dac46bd5a172886c26a56a7335d512
clim <cp.lim@rea-group.com>
2015-05-18 21:12:54 +1000

    04-creating-object-stores: Implemented.

--- offline-todo/application.js
+++ offline-todo/application.js
@@ -10,11 +10,21 @@
     return new Promise(function(resolve, reject) {
       var version = 1;
       var request = indexedDB.open('todos', version);
+
+      // run migrations (if necessary)
+      request.onupgradeneeded = function(e) {
+        db = e.target.result;
+        e.target.transaction.onerror = reject;
+        db.createObjectStore('todo', { keyPath: '_id' });
+      };
+
+      // resolve
       request.onsuccess = function(e) {
         db = e.target.result;
         resolve();
       };
 
+      // reject
       request.onerror = reject;
     });
   }
