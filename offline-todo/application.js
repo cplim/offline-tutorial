09112d751e5fb563ec4f07642ccb1ffd811777d6
clim <cp.lim@rea-group.com>
2015-05-18 21:40:43 +1000

    Migrations for v2.

--- offline-todo/application.js
+++ offline-todo/application.js
@@ -8,14 +8,28 @@
 
   function databaseOpen() {
     return new Promise(function(resolve, reject) {
-      var version = 1;
+      var version = 2;
       var request = indexedDB.open('todos', version);
 
       // run migrations (if necessary)
       request.onupgradeneeded = function(e) {
         db = e.target.result;
         e.target.transaction.onerror = reject;
-        db.createObjectStore('todo', { keyPath: '_id' });
+
+        switch(e.newVersion) {
+          case 2:
+            console.log('creating todoList');
+            db.createObjectStore('todoList', { keyPath: '_id' });
+            if(e.oldVersion === 1) { // remove old objectstore
+              console.log('deleting todo');
+              db.deleteObjectStore('todo');
+            }
+            break;
+
+          default:
+            console.log('creating todo');
+            db.createObjectStore('todo', { keyPath: '_id' });
+        }
       };
 
       // resolve
