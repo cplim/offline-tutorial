73d8a42d76ca7ceaad03b93e89a36e7fcb87d598
clim <cp.lim@rea-group.com>
2015-05-18 22:08:20 +1000

    06-adding-data: Implemented.

--- offline-todo/application.js
+++ offline-todo/application.js
@@ -1,10 +1,32 @@
 (function() {
   'use strict';
-  var db;
+  var db, input;
 
   databaseOpen().then(function() {
-    alert("The database has been opened");
+    input = document.querySelector('input');
+    document.body.addEventListener('submit', onSubmit);
   });
+
+  function onSubmit(e) {
+    e.preventDefault();
+    var todo = {
+      text: input.value,
+      _id: String(Date.now())
+    };
+    databaseTodosPut(todo).then(function() {
+      input.value = '';
+    });
+  }
+
+  function databaseTodosPut(todo) {
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction(['todo'], 'readwrite');
+      var store = transaction.objectStore('todo');
+      var request = store.put(todo);
+      transaction.oncomplete = resolve;
+      request.onerror = reject;
+    });
+  }
 
   function databaseOpen() {
     return new Promise(function(resolve, reject) {
